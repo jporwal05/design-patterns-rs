207a476e1b2a4984a8f67d398712f7ae90e03336
jporwal05 <jayantporwal05@gmail.com>
2023-11-06 23:52:22 +0530

    chore: Readability and comments

--- src/strategy/bin/main.rs
+++ src/strategy/bin/main.rs
@@ -1,9 +1,13 @@
 fn main() {
     // let payment_processor = PaymentProcessor::new(Box::new(CreditCard::new()));
     let payment_processor = PaymentProcessor::new(Box::new(Wallet::new()));
-    println!("{}", payment_processor.process(100.0));
+    println!(
+        "Total amount after processing fee is: {}",
+        payment_processor.process(100.0)
+    );
 }
 
+/// Payment processor utilizing a mode of payment, [PaymentMode] to process a payment.
 pub struct PaymentProcessor {
     pub payment_mode: Box<dyn PaymentMode>,
 }
@@ -18,6 +22,8 @@ impl PaymentProcessor {
     }
 }
 
+/// This is a strategy. In this example's context it is simply a mode of payment using which
+/// one can process a payment or pay.
 pub trait PaymentMode {
     fn process(&self, amount: f32) -> f32;
 }
@@ -46,14 +52,16 @@ impl Wallet {
     }
 }
 
+/// This is an implementation of [PaymentMode]. In this example's context it is a credit card.
 impl PaymentMode for CreditCard {
     fn process(self: &CreditCard, amount: f32) -> f32 {
-        amount - ((self.processing_fee / 100.0) * amount)
+        amount + ((self.processing_fee / 100.0) * amount)
     }
 }
 
+/// This is an implementation of [PaymentMode]. In this example's context it is an e-wallet.
 impl PaymentMode for Wallet {
     fn process(&self, amount: f32) -> f32 {
-        amount - ((self.processing_fee / 100.0) * amount)
+        amount + ((self.processing_fee / 100.0) * amount)
     }
 }
